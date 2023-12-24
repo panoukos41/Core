f27d65604f3a27c22acb189c858442842a623960
Panos <panos_ath@outlook.com>
2023-12-24 17:52:56 +0200

    update: Address.cs

--- src/Core/Primitives/Address.cs
+++ src/Core/Primitives/Address.cs
@@ -9,17 +9,22 @@ public readonly record struct Address :
     IEquatable<Address>,
     IEquatable<Address?>
 {
-    public required string Street { get; init; }
+    public string? Street { get; init; }
 
-    public required string Region { get; init; }
+    public string? Region { get; init; }
 
-    public required string ZipCode { get; init; }
+    public string? ZipCode { get; init; }
 
-    public required string City { get; init; }
+    public string? City { get; init; }
 
-    public required string Country { get; init; }
+    public string? Country { get; init; }
 
-    public required Location Location { get; init; }
+    public Location? Location { get; init; }
+
+    public override string ToString()
+    {
+        return string.Join(", ", Street, Region, ZipCode, City, Country);
+    }
 
     public bool Equals(Address? other)
     {
@@ -38,10 +43,34 @@ public readonly record struct Address :
         City = string.Empty,
         Country = string.Empty,
         ZipCode = string.Empty,
-        Location = Location.Empty
+        Location = Primitives.Location.Empty
     };
 
     public static IValidator<Address> Validator { get; } = InlineValidator.For<Address>(data =>
     {
+        data.RuleFor(x => x.Street)
+            .Length(1, 100)
+            .When(x => x.Street is { });
+
+        data.RuleFor(x => x.Region)
+            .Length(1, 50)
+            .When(x => x.Region is { });
+
+        data.RuleFor(x => x.ZipCode)
+            .Length(1, 50)
+            .When(x => x.ZipCode is { });
+
+        data.RuleFor(x => x.City)
+            .Length(1, 50)
+            .When(x => x.City is { });
+
+        data.RuleFor(x => x.Country)
+            .Length(1, 50)
+            .When(x => x.Country is { });
+
+        data.RuleFor(x => x.Location!.Value)
+            .SetValidator(Primitives.Location.Validator)
+            .OverridePropertyName(nameof(Address.Location))
+            .When(x => x.Location is { });
     });
 }
