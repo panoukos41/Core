cb7ed0d537dd219a8ba812e373ab14976c185dc3
Panos <panos_ath@outlook.com>
2023-12-29 02:07:17 +0200

    add: ClaimsPrincipalMixins

---
+++ src/Core/Common/Mixins/ClaimsPrincipalMixins.cs
@@ -0,0 +1,11 @@
+﻿namespace System.Security.Claims;
+
+public static class ClaimsPrincipalMixins
+{
+    public static T? FindFirst<T>(this ClaimsPrincipal user, string type)
+        where T : IParsable<T>
+    {
+        var value = user.FindFirst(type)?.Value;
+        return T.TryParse(value, null, out var result) ? result : default;
+    }
+}
