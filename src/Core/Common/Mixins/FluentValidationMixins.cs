e9efaa7cfe3d4204a423d4ee43db91263cf160b4
Panos <panos_ath@outlook.com>
2023-12-29 02:07:45 +0200

    refactor: FluetValidationMixins

--- src/Core/Common/Mixins/FluentValidationMixins.cs
+++ src/Core/Common/Mixins/FluentValidationMixins.cs
@@ -4,20 +4,20 @@ namespace FluentValidation;
 
 public static class FluentValidationMixins
 {
-    public static IRuleBuilder<T, string> Email<T>(this IRuleBuilder<T, string> builder)
+    public static IRuleBuilderOptions<T, string> Email<T>(this IRuleBuilder<T, string> builder)
         => builder
         .NotEmpty()
         .EmailAddress()
         .Length(3, 25)
         .WithName("Email");
 
-    public static IRuleBuilder<T, string> Password<T>(this IRuleBuilder<T, string> builder)
+    public static IRuleBuilderOptions<T, string> Password<T>(this IRuleBuilder<T, string> builder)
         => builder
         .NotEmpty()
         .MinimumLength(10)
         .WithName("Password");
 
-    public static IRuleBuilder<T, TValid> Valid<T, TValid>(this IRuleBuilder<T, TValid> builder)
+    public static IRuleBuilderOptions<T, TValid> Valid<T, TValid>(this IRuleBuilder<T, TValid> builder)
         where TValid : IValid<TValid>
         => builder.SetValidator(TValid.Validator);
 }
