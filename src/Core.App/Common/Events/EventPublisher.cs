6a52b836451ea2a3b414ca026251486bfa8a23a4
Panos <panos_ath@outlook.com>
2024-01-13 11:17:30 +0200

    add: IEventPublisher, IEventSubscriber implementation

---
+++ src/Core.App/Common/Events/EventPublisher.cs
@@ -0,0 +1,31 @@
+﻿using System.Reactive;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace Core.Common.Events;
+
+public sealed class EventPublisher : ObservableBase<object>, IEventPublisher, IEventSubscriber, IDisposable
+{
+    private readonly Subject<object> _events = new();
+
+    public IObservable<object> Events { get; }
+
+    public EventPublisher()
+    {
+        Events = _events.AsObservable();
+    }
+
+    public void Publish<TEvent>(TEvent @event) where TEvent : class
+    {
+        _events.OnNext(@event);
+    }
+    protected override IDisposable SubscribeCore(IObserver<object> observer)
+    {
+        return Events.Subscribe(observer);
+    }
+
+    public void Dispose()
+    {
+        _events.Dispose();
+    }
+}
