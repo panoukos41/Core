5a764df5d9ffcf0991537c5feaed08959629b84b
Panos <panos_ath@outlook.com>
2024-01-14 00:19:32 +0200

    remove: EventHandler.cs

--- src/Core.App/Abstractions/Handlers/EventHandler.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Core.Abstractions.Handlers;
-
-public abstract class EventHandler<TEvent> : IEventHandler<TEvent> where TEvent : class
-{
-    public abstract ValueTask Handle(TEvent @event, CancellationToken cancellationToken);
-}
