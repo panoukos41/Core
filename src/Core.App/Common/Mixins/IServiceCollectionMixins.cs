7aeec562b73525f773fdee698358bbeb9e1ebb94
Panos <panos_ath@outlook.com>
2024-01-14 14:58:24 +0200

    add: IServiceCollectionMixins

---
+++ src/Core.App/Common/Mixins/IServiceCollectionMixins.cs
@@ -0,0 +1,12 @@
+﻿namespace Microsoft.Extensions.DependencyInjection;
+
+public static class IServiceCollectionMixins
+{
+    public static IServiceCollection AddEventHandler<TEvent, THandler>(this IServiceCollection services, ServiceLifetime serviceLifetime = ServiceLifetime.Scoped)
+        where TEvent : class
+        where THandler : IEventHandler<TEvent>
+    {
+        services.Add(new ServiceDescriptor(typeof(IEventHandler<TEvent>), typeof(THandler), serviceLifetime));
+        return services;
+    }
+}
