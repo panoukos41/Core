bcd71b3658b95290dc502e65637274be1023f649
Panos <panos_ath@outlook.com>
2024-04-07 16:44:47 +0300

    add: Core.Blazor project

---
+++ src/Core.Blazor/Components/CoreComponent.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Runtime.CompilerServices;
+
+namespace Core.Blazor.Components;
+
+public abstract class CoreComponent : ComponentBase
+{
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IDictionary<string, object?>? Attributes { get; set; }
+
+    public object? TryGetAttribute(string key)
+        => Attributes?.TryGetValue(key, out var value) is true ? value : null;
+
+    public T? TryGetAttribute<T>(string key) where T : class
+        => TryGetAttribute(key) as T;
+
+    public bool TryGetAttribute(string key, out object attribute)
+    {
+        Unsafe.SkipInit(out attribute);
+        if (Attributes?.TryGetValue(key, out var value) is true && value is { })
+        {
+            attribute = value;
+            return true;
+        }
+        return false;
+    }
+
+    public bool TryGetAttribute<T>(string key, out T attribute) where T : class
+    {
+        Unsafe.SkipInit(out attribute);
+        if (TryGetAttribute(key, out var value) && value is T valueAttr)
+        {
+            attribute = valueAttr;
+            return true;
+        }
+        return false;
+    }
+}
