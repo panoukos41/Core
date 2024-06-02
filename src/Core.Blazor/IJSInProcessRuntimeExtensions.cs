18c3f9c1bb2034f00c19a369d38f8ef289501148
Panos <panos_ath@outlook.com>
2024-06-02 18:50:33 +0300

    add: GetBroswerCulture method for IJSInProcessRuntime

---
+++ src/Core.Blazor/IJSInProcessRuntimeExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.JSInterop;
+
+namespace Core.Blazor;
+
+public static class IJSInProcessRuntimeExtensions
+{
+    /// <summary>
+    /// Returns the language code name from the browser, e.g. "de"
+    /// </summary>
+    public static string? GetBrowserLang(this IJSInProcessRuntime? js)
+    {
+        var language = GetBrowserCulture(js);
+        if (language is null) return null;
+
+        if (language.Contains('-'))
+        {
+            language = language.Split('-')[0];
+        }
+        if (language.Contains('_'))
+        {
+            language = language.Split('_')[0];
+        }
+        return language;
+    }
+
+    /// <summary>
+    /// Returns the culture language code name from the browser, e.g. "de-DE"
+    /// </summary>
+    public static string? GetBrowserCulture(this IJSInProcessRuntime? js)
+    {
+        using var navigator = js?.Invoke<IJSInProcessObjectReference>("navigator.valueOf");
+        if (navigator is null) return null;
+
+        var language = navigator.Invoke<string[]>("languages.valueOf")?.ElementAtOrDefault(0);
+        language ??= navigator.Invoke<string>("language.valueOf");
+        language ??= navigator.Invoke<string>("browserLanguage.valueOf");
+        language ??= navigator.Invoke<string>("userLanguage.valueOf");
+        return language;
+    }
+}
