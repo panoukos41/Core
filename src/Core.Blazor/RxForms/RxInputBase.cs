a16f0f058066fd50aa776b63b8358cd66d47d6eb
Panos <panos_ath@outlook.com>
2024-06-15 02:59:45 +0300

    add(forms): rx components based on reactive forms

---
+++ src/Core.Blazor/RxForms/RxInputBase.cs
@@ -0,0 +1,165 @@
+﻿using Core.Blazor.Reactive.Forms;
+using Core.Blazor.Reactive.Forms.Events;
+using Core.Blazor.Reactive.Forms.Primitives;
+using Ignis.Components;
+using Microsoft.AspNetCore.Components;
+
+namespace Core.Blazor.RxForms;
+
+public abstract class RxInputBase<T> : IgnisComponentBase, IDisposable where T : IParsable<T>
+{
+    private FormControl<T>? __formControl;
+    private IDisposable? subscription;
+
+    public FormControl<T> FormControl
+    {
+        get {
+            if (__formControl is { }) return __formControl;
+
+            if (FormGroup?.TryGet(Id, out __formControl) is false)
+            {
+                FormGroup.Add(Id, __formControl ??= new());
+                return __formControl;
+            }
+            if (FormGroupParent?.TryGet(Id, out __formControl) is false)
+            {
+                FormGroupParent.Add(Id, __formControl ??= new());
+                return __formControl;
+            }
+            return __formControl ??= new();
+        }
+    }
+
+    public string CssClass => FormControl.Status switch
+    {
+        ControlStatus.Valid => Class("valid"),
+        ControlStatus.Pending => Class("pending"),
+        ControlStatus.Invalid => Class("invalid"),
+        ControlStatus.Disabled => Class("disabled"),
+        _ => string.Empty
+    };
+
+    /// <summary>
+    /// Gets or sets a collection of additional attributes that will be applied to the created element.
+    /// </summary>
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IDictionary<string, object?>? Attributes { get; set; }
+
+    [CascadingParameter]
+    public FormGroup? FormGroupParent { get; set; }
+
+    [Parameter]
+    public FormGroup? FormGroup { get; set; }
+
+    [Parameter]
+    public string Id { get; set; } = string.Empty;
+
+    [Parameter]
+    public string Label { get; set; } = string.Empty;
+
+    [Parameter]
+    public T? Value { get; set; }
+
+    [Parameter]
+    public EventCallback<T?> ValueChanged { get; set; }
+
+    [Parameter]
+    public bool Read { get; set; }
+
+    [Parameter]
+    public HashSet<ValidatorFn>? Validators { get; set; }
+
+    [Parameter]
+    public HashSet<ValidatorFnAsync>? ValidatorsAsync { get; set; }
+
+    [Parameter]
+    public bool Disabled
+    {
+        get => FormControl.Disabled;
+        set {
+            if (value)
+            {
+                FormControl.ClearErrors();
+                FormControl.Disable();
+            }
+            else
+                FormControl.Enable();
+        }
+    }
+
+    [Parameter]
+    public bool HideErrors { get; set; }
+
+    protected override void OnInitialized()
+    {
+        if (string.IsNullOrWhiteSpace(Id))
+        {
+            Id = $"rx-{Guid.NewGuid():N}";
+        }
+
+        subscription = FormControl.Events.Subscribe(e =>
+        {
+            if (e is ValueChangeEvent<T> valueChange)
+            {
+                Value = valueChange.Value;
+                ValueChanged.InvokeAsync(Value);
+                Update();
+            }
+        });
+    }
+
+    protected override void OnUpdate()
+    {
+        // If values are not the same this was an update to the [Parameter] so update the control.
+        if (!EqualityComparer<T>.Default.Equals(Value, FormControl.Value))
+        {
+            FormControl.Value = Value;
+        }
+        // Update validators based on component.
+        if (Validators?.SetEquals(FormControl.Validators) is false)
+        {
+            var toRemove = FormControl.Validators.Except(Validators);
+            foreach (var validator in toRemove)
+            {
+                FormControl.RemoveValidator(validator);
+            }
+            foreach (var validator in Validators)
+            {
+                FormControl.AddValidator(validator);
+            }
+            FormControl.Validate();
+        }
+        // Update validators based on component.
+        if (ValidatorsAsync?.SetEquals(FormControl.ValidatorsAsync) is false)
+        {
+            var toRemove = FormControl.ValidatorsAsync.Except(ValidatorsAsync);
+            foreach (var validator in toRemove)
+            {
+                FormControl.RemoveValidator(validator);
+            }
+            foreach (var validator in ValidatorsAsync)
+            {
+                FormControl.AddValidator(validator);
+            }
+            FormControl.Validate();
+        }
+    }
+
+    public void Dispose()
+    {
+        FormGroupParent?.Remove(Id);
+        subscription?.Dispose();
+        subscription = null;
+        GC.SuppressFinalize(this);
+    }
+
+    private string Class(string always) => string.Join(' ', always, Class());
+
+    public string? Class() => TryGetAttribute<string>("class");
+
+    public object? TryGetAttribute(string key)
+        => Attributes?.TryGetValue(key, out var value) is true ? value : null;
+
+    public TAttribute? TryGetAttribute<TAttribute>(string key)
+        => TryGetAttribute(key) is TAttribute v ? v : default;
+}
