3598bca992ab9e8877be835c1cd49e9d7194e0bb
Panos <panos_ath@outlook.com>
2024-07-13 19:08:51 +0300

    fix: UuidJsonConverter

--- src/Core/Common/JsonConverters/UuidJsonConverter.cs
+++ src/Core/Common/JsonConverters/UuidJsonConverter.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 
 namespace Core.Common.JsonConverters;
@@ -13,5 +14,15 @@ public sealed class UuidJsonConverter : JsonConverter<Uuid>
     public override void Write(Utf8JsonWriter writer, Uuid value, JsonSerializerOptions options)
     {
         writer.WriteRawValue(['"', .. value.ToString(), '"']);
+    }
+
+    public override Uuid ReadAsPropertyName(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        return Read(ref reader, typeToConvert, options);
+    }
+
+    public override void WriteAsPropertyName(Utf8JsonWriter writer, [DisallowNull] Uuid value, JsonSerializerOptions options)
+    {
+        writer.WritePropertyName(value.ToString());
     }
 }
