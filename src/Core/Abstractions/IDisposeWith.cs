fb8e5b081ae73eb48e6a8d2172f0f6354f748675
Panos <panos_ath@outlook.com>
2024-07-28 18:00:17 +0300

    add: IDisposeWith interface
    This interface means you can dispose extra disposables with the parent object.

---
+++ src/Core/Abstractions/IDisposeWith.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Abstractions;
+
+/// <summary>
+/// Indicates that a <see cref="IDisposable"/> object can dispose other objects with it.
+/// </summary>
+public interface IDisposeWith
+{
+    /// <summary>
+    /// Add a disposable that will be disposed with this instance.
+    /// </summary>
+    public void DisposeWith(IDisposable disposable);
+}
--- src/Core/Common/Mixins/IDisposableMixins.cs
+++ src/Core/Common/Mixins/IDisposableMixins.cs
@@ -1,7 +1,16 @@
-﻿namespace System;
+﻿using Core.Abstractions;
+
+namespace System;
 
 public static class IDisposableMixins
 {
+    public static TDisposable DisposeWith<TDisposable>(this TDisposable disposable, IDisposeWith parent)
+        where TDisposable : IDisposable
+    {
+        parent.DisposeWith(disposable);
+        return disposable;
+    }
+
     public static TDisposable DisposeWith<TDisposable>(this TDisposable disposable, ICollection<IDisposable> disposables)
         where TDisposable : IDisposable
     {
