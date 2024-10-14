0509fa90a4df91c6ee06b001f85139e569a80ede
Panos <panos_ath@outlook.com>
2024-10-14 19:51:55 +0300

    add(EnumerableExtensions): Do method that behaves like reactive Do

--- src/Core/Common/Mixins/EnumerableMixins.cs
+++ src/Core/Common/Mixins/EnumerableMixins.cs
@@ -6,4 +6,13 @@ public static class EnumerableMixins
     {
         return values.Aggregate(0, static (a, c) => HashCode.Combine(a, c));
     }
+
+    public static IEnumerable<T> Do<T>(this IEnumerable<T> values, Action<T> action)
+    {
+        foreach (var item in values)
+        {
+            action(item);
+            yield return item;
+        }
+    }
 }
